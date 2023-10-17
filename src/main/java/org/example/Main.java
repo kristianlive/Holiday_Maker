8af978bb96f915ea94142a33bb17ab3de22bb182
NenadJovcic <nenad.hif@hotmail.se>
2023-10-17 15:13:45 +0200

    Trip service, tripRepo. Able to create update, remove

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,27 @@
 package org.example;
 
 import org.example.db.Database;
-import org.example.entity.User;
-import org.example.repository.userRepo.UserRepositoryImp;
-import org.example.services.UserService;
+import org.example.entity.Accomodation;
+import org.example.entity.Trip;
+import org.example.entity.TypeOfTrip;
+import org.example.repository.tripRepo.TripRepositoryImp;
+import org.example.services.TripService;
+
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
         Database db = new Database();
         db.connectToDb();
-        UserService userService = new UserService(new UserRepositoryImp());
-        userService.findById(1L);
-        userService.getAllUsers();
 
-        userService.addUser(User.builder()
-                .firstName("David")
-                .lastName("Davidsson")
-                .email("david.dav@example.com")
-                .password("123456")
-                .build());
-        userService.getAllUsers();
-        userService.updateUser(User.builder()
-                .id(1)
-                .firstName("David")
-                .lastName("Reed")
-                .email("david.reed@hotmail.com")
-                .password("123456")
-                .build());
-        userService.getAllUsers();
-        userService.removeUser(1L);
-        userService.getAllUsers();
+
+
+
+
+
+
+
 
 
     }
--- src/main/java/org/example/repository/tripRepo/TripRepositoryImp.java
+++ src/main/java/org/example/repository/tripRepo/TripRepositoryImp.java
@@ -1,32 +1,120 @@
 package org.example.repository.tripRepo;
 
+import org.example.db.Database;
 import org.example.entity.Trip;
+import org.example.entity.TypeOfTrip;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TripRepositoryImp implements TripRepository {
+    Database db = new Database();
+    Connection conn = db.connectToDb();
+    Statement stmt = null;
+    ResultSet rs = null;
     @Override
     public Trip get(Long id) {
+        String sql = "SELECT * FROM trips WHERE trip_id = ?";
+        try (Connection conn = db.connectToDb();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                Trip trip = new Trip();
+                trip.setId(rs.getLong("trip_id"));
+                trip.setName(rs.getString("name"));
+                trip.setType(TypeOfTrip.valueOf(rs.getString("type")));
+                trip.setPrice(rs.getDouble("price"));
+                return trip;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     @Override
     public void add(Trip trip) {
+        String sql = "INSERT INTO trips (name, price, type) VALUES (?, ?, ?)";
+        try (Connection conn = db.connectToDb();
+             PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, trip.getName());
+            stmt.setDouble(2, trip.getPrice());
+            stmt.setString(3, trip.getType().toString());
 
+            int affectedRows = stmt.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("Creating trip failed, no rows affected.");
+            }
+
+            try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    trip.setId(generatedKeys.getLong(1));
+                } else {
+                    throw new SQLException("Creating trip failed, no ID obtained.");
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void update(Trip trip) {
+        String sql = "UPDATE trips SET name = ?, price = ?, type = ? WHERE trip_id = ?";
+        try (Connection conn = db.connectToDb();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, trip.getName());
+            stmt.setDouble(2, trip.getPrice());
+            stmt.setString(3, trip.getType().toString());
+            stmt.setLong(4, trip.getId());
 
+            int affectedRows = stmt.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("Updating trip failed, no rows affected.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
+
+
 
     @Override
     public void remove(Trip trip) {
-
+        String sql = "DELETE FROM trips WHERE trip_id = ?";
+        try (Connection conn = db.connectToDb();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, trip.getId());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    @Override
-    public List<Trip> getAllTrips() {
-        return null;
-    }
+
+   @Override
+   public List<Trip> getAllTrips() {
+       List<Trip> trips = new ArrayList<>();
+       String sql = "SELECT * FROM trips";
+       try (Connection conn = db.connectToDb();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)) {
+           while (rs.next()) {
+               Trip trip = Trip.builder()
+                       .id(rs.getLong("trip_id"))
+                       .name(rs.getString("name"))
+                       .type(TypeOfTrip.valueOf(rs.getString("type")))
+                       .price(rs.getDouble("price"))
+                       .build();
+
+               trips.add(trip);
+           }
+       } catch (SQLException e) {
+           System.out.println(e.getMessage());
+       }
+       return trips;
+   }
+
 }
