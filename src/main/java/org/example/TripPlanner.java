213ce9a7121774cfed298b003907eb4470006f51
AdamAksel <adamcarlssoncoding@gmail.com>
2023-10-24 09:30:12 +0200

    commit to pull new

--- src/main/java/org/example/TripPlanner.java
+++ src/main/java/org/example/TripPlanner.java
@@ -18,6 +18,8 @@ import java.util.Scanner;
 
 public class TripPlanner {
 
+    private int currentUser;
+
     private Scanner scanner;
 
     public TripPlanner() {
@@ -37,7 +39,7 @@ public class TripPlanner {
         System.out.println("Welcome to the Trip Planner!");
         System.out.println("-----------------------------");
 
-        /*registerUser();*/
+        registerUser();
         mainMenu();
     }
 
@@ -58,9 +60,10 @@ public class TripPlanner {
 
         // Call the method to store the user information in the database.
         // saveUserInfo(firstName, lastName, email, password);
-        userService.addUser(User.builder().firstName(firstName).lastName(lastName).email(email).password(password).build());
+        currentUser = userService.addUser(User.builder().firstName(firstName).lastName(lastName).email(email).password(password).build());
         System.out.println("Registration Successful!");
         System.out.println("-----------------------------");
+        System.out.println(currentUser);
         userService.getAllUsers();
     }
 
